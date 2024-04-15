0eb5ac318ca0bcbf47eecf08586d80d5364cdd38
jmbae <jmbae@codegrove.co.kr>
2024-04-15 16:26:53 +0900

    [feat] UI

--- OXQuiz/OXQuiz/ContentView.swift
+++ OXQuiz/OXQuiz/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: Int = 4
+    @State var number2: Int = 2
+    @State var resultNumber: Int = 9
+    
+    @State var countCorrect: Int = 0
+    @State var countWrong: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("다음 수식은 맞을까요?")
+            Spacer()
+            Text("\(number1) x \(number2) = \(resultNumber)")
+            Spacer()
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "checkmark.diamond.fill")
+                        Text("맞음")
+                            .bold()
+                    }
+                    .foregroundStyle(.green)
+                }
+                Spacer()
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "xmark.diamond")
+                        Text("틀림")
+                            .bold()
+                    }
+                    .foregroundStyle(.red)
+                }
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Text("\(countCorrect)개 맞춤")
+                Text("\(countWrong)개 틀림")
+            }
+            Spacer()
+            Button("카운트 초기화") {
+                countCorrect = 0
+                countWrong = 0
+            }
         }
+        .font(.largeTitle)
         .padding()
     }
 }
