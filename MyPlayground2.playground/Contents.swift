34fa8dbfdf11df2d8ec37cb5eff6908ba3058952
jmbae <jmbae@codegrove.co.kr>
2024-04-15 11:16:05 +0900

    opaque return type

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -21,3 +21,22 @@ class MyClass: MessageBuilder {
         "Hello" + name
     }
 }
+
+func doubleFunc1 (value: Int) -> some Equatable {
+    return value * 2
+}
+
+func doubleFunc2 (value: String) -> some Equatable {
+    value + value
+}
+
+
+let intOne = doubleFunc1(value: 10)
+print(intOne)
+let stringOne = doubleFunc2(value: "Hello")
+print(stringOne)
+
+// 동작하지 않는다
+//if (intOne == stringOne) {
+//    print("They match")
+//}
