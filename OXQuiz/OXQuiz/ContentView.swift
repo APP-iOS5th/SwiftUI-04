a8f9f6e7d0c37137ef0a44904d29a3ae56a38695
jmbae <jmbae@codegrove.co.kr>
2024-04-15 16:14:09 +0900

    initialize project (add OXQuiz files)

---
+++ OXQuiz/OXQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OXQuiz
+//
+//  Created by Jungman Bae on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
