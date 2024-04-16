98403323a4bc29a9e5c1650452a41107dda89431
jmbae <jmbae@codegrove.co.kr>
2024-04-16 10:00:58 +0900

    Struct Enum

--- MyPlayground12.playground/Contents.swift
+++ MyPlayground12.playground/Contents.swift
@@ -31,3 +31,25 @@ myClass2.name = "David"
 
 print(myClass1.name)
 print(myClass2.name)
+
+
+enum Temperature {
+    case hot
+    case warm
+    case cold(cetigrade: Int)
+}
+
+func displayTempInfo(temp: Temperature) {
+    switch temp {
+    case .hot:
+        print("It is hot.")
+    case .warm:
+        print("It is warm.")
+    case .cold(let centigrade) where centigrade <= 0:
+        print("Ice warning: \(centigrade) degrees.")
+    case .cold:
+        print("It is cold.")
+    }
+}
+
+displayTempInfo(temp: .cold(cetigrade: -10))
