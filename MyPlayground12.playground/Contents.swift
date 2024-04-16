e521a8606746ffeec65e78d49bc3c03bc2893f0f
jmbae <jmbae@codegrove.co.kr>
2024-04-16 09:44:40 +0900

    Struct Class

--- MyPlayground12.playground/Contents.swift
+++ MyPlayground12.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+struct SampleStruct {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    func buildHelloMsg() {
+        "Hello " + name
+    }
+}
 
-var greeting = "Hello, playground"
+class SampleClass {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+    func buildHelloMsg() {
+        "Hello " + name
+    }
+}
+
+let myStruct1 = SampleStruct(name: "Mark")
+var myStruct2 = myStruct1
+myStruct2.name = "David"
+
+print(myStruct1.name)
+print(myStruct2.name)
+
+let myClass1 = SampleClass(name: "Mark")
+var myClass2 = myClass1
+myClass2.name = "David"
+
+print(myClass1.name)
+print(myClass2.name)
