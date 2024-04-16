0e24dd5df49a4685b1e5be837331f8260598bead
jmbae <jmbae@codegrove.co.kr>
2024-04-16 15:41:35 +0900

    make Classes Car..

--- CarList/CarList/ContentView.swift
+++ CarList/CarList/ContentView.swift
@@ -7,6 +7,80 @@
 
 import SwiftUI
 
+class Car: Identifiable {
+    let id = UUID()
+    var brand: String
+    var modelName: String
+    var year: Int
+    var doorCount: Int
+    var weight: Double
+    var height: Double
+    
+    init(brand: String, modelName: String, year: Int, doorCount: Int,
+         weight: Double, height: Double) {
+        self.brand = brand
+        self.modelName = modelName
+        self.year = year
+        self.doorCount = doorCount
+        self.weight = weight
+        self.height = height
+    }
+}
+
+class ElectricCar: Car {
+    var electricEfficiency: Double
+    var fullChargeHours: Double
+    var autoLevel: Int
+    
+    init(brand: String, modelName: String, year: Int, doorCount: Int, weight: Double, height: Double,
+         electricEfficiency: Double, fullChargeHours: Double, autoLevel: Int) {
+        self.electricEfficiency = electricEfficiency
+        self.fullChargeHours = fullChargeHours
+        self.autoLevel = autoLevel
+        super.init(brand: brand, modelName: modelName, year: year, doorCount: doorCount,
+                   weight: weight, height: height)
+    }
+}
+
+class OilCar: Car {
+    var isAutomatic: Bool
+    var fuelEfficiency: Double
+    var isGasoline: Bool
+    
+    init(brand: String, modelName: String, year: Int, doorCount: Int, weight: Double, height: Double,
+         isAutomatic: Bool, fuelEfficiency: Double, isGasoline: Bool) {
+        self.isAutomatic = isAutomatic
+        self.fuelEfficiency = fuelEfficiency
+        self.isGasoline = isGasoline
+        super.init(brand: brand, modelName: modelName, year: year, doorCount: doorCount, weight: weight, height: height)
+        
+    }
+}
+
+class HybridCar: Car {
+    var fuelEfficiency: Double
+    var isGasoline: Bool
+    var autoLevel: Int
+    
+    init(brand: String, modelName: String, year: Int, doorCount: Int, weight: Double, height: Double,
+         fuelEfficiency: Double, isGasoline: Bool, autoLevel: Int) {
+        self.fuelEfficiency = fuelEfficiency
+        self.isGasoline = isGasoline
+        self.autoLevel = autoLevel
+        super.init(brand: brand, modelName: modelName, year: year, doorCount: doorCount, weight: weight, height: height)
+    }
+}
+
+
+let teslaModelX: ElectricCar = ElectricCar(brand: "Tesla", modelName: "Model X", year: 2015, doorCount: 4, weight: 100, height: 40, electricEfficiency: 439, fullChargeHours: 5, autoLevel: 2)
+let teslaModelY: ElectricCar = ElectricCar(brand: "Tesla", modelName: "Model Y", year: 2015, doorCount: 4, weight: 100, height: 40, electricEfficiency: 511 , fullChargeHours: 7, autoLevel: 3)
+let kiaK9: OilCar = OilCar(brand: "KIA", modelName: "K9", year: 2022, doorCount: 4, weight: 70, height: 35, isAutomatic: true, fuelEfficiency: 9.0, isGasoline: true)
+let kiaK8: OilCar = OilCar(brand: "KIA", modelName: "K8", year: 2023, doorCount: 4, weight: 70, height: 35, isAutomatic: true, fuelEfficiency: 18.0, isGasoline: true)
+let kiaK5: OilCar = OilCar(brand: "KIA", modelName: "K5", year: 2023, doorCount: 4, weight: 70, height: 35, isAutomatic: true, fuelEfficiency: 20.0, isGasoline: true)
+let prius: HybridCar = HybridCar(brand: "Toyota", modelName: "Prius", year: 2023, doorCount: 4, weight: 60, height: 40, fuelEfficiency: 65, isGasoline: true, autoLevel: 0)
+let grandeurHybrid: HybridCar = HybridCar(brand: "Hyundai", modelName: "그렌저 하이브리드", year: 2023, doorCount: 4, weight: 70, height: 35, fuelEfficiency: 16.7, isGasoline: true, autoLevel: 1)
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
