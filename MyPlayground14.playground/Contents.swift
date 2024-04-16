89375d4fe311bcd5918618876e3682abcf7a9662
jmbae <jmbae@codegrove.co.kr>
2024-04-16 11:43:58 +0900

    Dictionary

--- MyPlayground14.playground/Contents.swift
+++ MyPlayground14.playground/Contents.swift
@@ -20,3 +20,24 @@ treeArray.shuffle()
 
 print(treeArray)
 print(treeArray.randomElement() ?? "")
+
+
+
+//var bookDict: [String:String] = ["100-432112": "Wind in the Willows"]
+var bookDict1 = ["100-432112": "Wind in the Willows"]
+// 빈 딕셔너리 초기화 정수형 키값, 문자열
+var myDictionary = [Int: String]()
+
+let keys = ["1","2","3","4","5","6","7"]
+let values = ["Value1","Value2","Value3","Value4","Value5","Value6","Value7",]
+
+//let bookDict = Dictionary(uniqueKeysWithValues: zip(keys, values))
+var bookDict = Dictionary(uniqueKeysWithValues: zip(1..., values))
+
+print(bookDict.count)
+print(bookDict[1, default: "NoValue"] )
+bookDict[1231000] = "Value"
+
+for (bookId, value) in bookDict {
+    print("Book Id: \(bookId) Value: \(value)")
+}
