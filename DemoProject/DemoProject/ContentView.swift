b19506822ea2434c46d455ead6ce7f2258af1b42
jmbae <jmbae@codegrove.co.kr>
2024-04-18 08:57:59 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,24 +7,10 @@
 
 import SwiftUI
 
-struct StandardTitle: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .background(Color.white)
-            .border(Color.gray, width: 0.2)
-            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Text 1")
-                .modifier(StandardTitle())
-            Spacer()
-            Text("Text 2")
-                .modifier(StandardTitle())
+
         }
     }
 }
