26e8e8fb7e5cf5e792dd3f2224e70c69b9da8d70
jmbae <jmbae@codegrove.co.kr>
2024-04-17 13:03:23 +0900

    Initialize DemoProject

---
+++ DemoProject/DemoProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by Jungman Bae on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by Jungman Bae on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
