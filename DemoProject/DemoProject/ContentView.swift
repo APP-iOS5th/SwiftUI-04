00f6a4925a604b8fd301c52833e9e5d83b12b485
jmbae <jmbae@codegrove.co.kr>
2024-04-17 14:45:09 +0900

    Custom modifier

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,14 +7,24 @@
 
 import SwiftUI
 
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Sample Text")
-                .font(.custom("Copperplate", size: 58))
-                .padding(20)
-                .border(Color.black)
-
+            Text("Text 1")
+                .modifier(StandardTitle())
+            Spacer()
+            Text("Text 2")
+                .modifier(StandardTitle())
         }
     }
 }
