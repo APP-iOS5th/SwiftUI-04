c9223058bcb80fd3b2aa53a7fd6bf864960e0b1f
jmbae <jmbae@codegrove.co.kr>
2024-04-17 14:41:19 +0900

    modifier order

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -8,16 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let carStack = HStack {
-        Text("Car Image")
-        Image(systemName: "car.fill")
-    }
-    
     var body: some View {
         VStack {
-            Text("Main Title")
-                .font(.largeTitle)
-            carStack
+            Text("Sample Text")
+                .font(.custom("Copperplate", size: 58))
+                .padding(20)
+                .border(Color.black)
+
         }
     }
 }
