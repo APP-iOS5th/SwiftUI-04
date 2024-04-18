088ffc0f877257491b1c7e6376a800ae32b796d8
jmbae <jmbae@codegrove.co.kr>
2024-04-18 10:27:22 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -10,24 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName:"goforward.10")
-                Image(systemName:"goforward.15")
-                Image(systemName:"goforward.30")
-            }
-            .padding()
-            
-            HStack(alignment: .top) {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-            }
+
         }
     }
 }
