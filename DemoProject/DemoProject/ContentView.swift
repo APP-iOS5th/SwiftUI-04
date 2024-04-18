3a2cf232de7ea3a09d4e5294219155c7ea26d57a
jmbae <jmbae@codegrove.co.kr>
2024-04-18 11:24:09 +0900

    frame modifier

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            
+            Text("Hello World, how are you?")
+                .font(.largeTitle)
+                .border(Color.black)
+//                .frame(minWidth: 100, maxWidth: 300, minHeight: 100, maxHeight: 100, alignment: .center)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
