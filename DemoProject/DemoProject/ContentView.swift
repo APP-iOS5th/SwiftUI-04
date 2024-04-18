ef4e80630e3ae41f8339a2751c1a003231747f3f
jmbae <jmbae@codegrove.co.kr>
2024-04-18 11:43:06 +0900

    State

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wifiEnabled = true
+    @State private var userName = ""
     var body: some View {
         VStack {
-
+            Toggle(isOn: $wifiEnabled) {
+                Text("Enable Wi-Fi")
+            }
+            TextField("Enter user name", text: $userName)
+            Text(userName)
+            Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
+                .font(.largeTitle)
         }
     }
 }
