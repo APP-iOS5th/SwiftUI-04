99746ac5ee455b6400b2ffe9988b1eafeef08781
jmbae <jmbae@codegrove.co.kr>
2024-04-18 10:45:03 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -8,32 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myLayout: AnyLayout = AnyLayout(VStackLayout())
-    
     var body: some View {
-        VStack {
-            myLayout {
-                Image(systemName: "goforward.10")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Image(systemName: "goforward.15")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            }
+        HStack {
             
-            HStack {
-                Button(action: {
-                    myLayout = AnyLayout(HStackLayout())
-                }) {
-                    Text("HStack")
-                }
-                Button(action: {
-                    myLayout = AnyLayout(VStackLayout())
-                }) {
-                    Text("VStack")
-                }
-
-            }
         }
     }
 }
