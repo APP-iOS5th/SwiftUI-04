6b90dfa2399efa9341e79e7dd3982d3aaa824d80
jmbae <jmbae@codegrove.co.kr>
2024-04-18 09:44:20 +0900

    Custom Container

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+struct MyVStack<Content: View>: View {
+    let content: () -> Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            content()
+        }
+        .font(.largeTitle)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-
+        MyVStack {
+            Text("Text Item 1")
+            Text("Text Item 2")
+            Text("Text Item 3")
+            HStack {
+                Image(systemName: "star.fill")
+                Image(systemName: "star.fill")
+                Image(systemName: "star")
+            }
         }
     }
 }
