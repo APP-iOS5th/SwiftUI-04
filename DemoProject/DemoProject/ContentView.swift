237b9ba650e2a110c8b54ec3f0385d09e2b62379
jmbae <jmbae@codegrove.co.kr>
2024-04-19 13:27:30 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -10,33 +10,8 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Button(action: {
-                doSomething()
-            }) {
-                Text("Do something")
-            }
+            
         }
-    }
-    
-    func doSomething() {
-        print("Start \(Date())")
-        let task = Task(priority: .background) {
-            let result = await takesTooLong()
-            print("Date result: \(result)")
-        }
-        print(task.isCancelled)
-        if !task.isCancelled {
-            task.cancel()
-        }
-        print(task.isCancelled)
-        print("End \(Date())")
-    }
-    
-    func takesTooLong() async -> Date {
-        print("tasksTooLong start \(Task.currentPriority)")
-        sleep(5)
-        print("tasksTooLong ")
-        return Date()
     }
 }
 
