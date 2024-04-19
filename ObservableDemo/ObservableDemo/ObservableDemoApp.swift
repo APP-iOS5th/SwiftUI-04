c15255491446ef34a849d5ffb8c7c657ba054424
jmbae <jmbae@codegrove.co.kr>
2024-04-19 17:11:46 +0900

    Intialize Project ObservableDemo

---
+++ ObservableDemo/ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by Jungman Bae on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -0,0 +1,8 @@
+//
+//  TimerData.swift
+//  ObservableDemo
+//
+//  Created by Jungman Bae on 4/19/24.
+//
+
+import Foundation
