201a83ba69fbefc161ed2c5166088f794d1e724b
jmbae <jmbae@codegrove.co.kr>
2024-04-19 11:22:13 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -4,31 +4,13 @@
 //
 //  Created by Jungman Bae on 4/17/24.
 //
-
 import SwiftUI
 
-class DemoData: ObservableObject {
-    @Published var userCount = 0
-    
-    func increment() {
-        userCount += 1
-    }
-    
-}
-
 struct ContentView: View {
-    @StateObject var demoData: DemoData = DemoData()
     
     var body: some View {
         VStack {
-            Text("you are user number \(demoData.userCount)")
-            
-            Button(action: {
-                demoData.increment()
-            }) {
-                Text("Increment")
-                    .font(.largeTitle)
-            }
+
         }
     }
 }
