95a6652217d706ab6876e0f2bace29aa54e05fe8
jmbae <jmbae@codegrove.co.kr>
2024-04-19 13:35:03 +0900

    Github Repositories UI

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -6,10 +6,34 @@
 //
 import SwiftUI
 
+struct Repository: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String?
+    let stargazersCount: Int
+}
+
 struct ContentView: View {
+    @State private var username = "jmbae"
+    @State private var repositories: [Repository] = []
     
     var body: some View {
         VStack {
+            TextField("Github username:", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button("Fetch Data") {
+                
+            }
+            List(repositories) { repo in
+                VStack(alignment: .leading) {
+                    Text(repo.name)
+                        .font(.headline)
+                    Text(repo.description ?? "No description")
+                        .font(.subheadline)
+                    Text("Stars: \(repo.stargazersCount)")
+                }
+            }
             
         }
     }
