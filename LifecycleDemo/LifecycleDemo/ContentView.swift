de80c56869ba0c33a25fb1abb21f704dceccaebd
jmbae <jmbae@codegrove.co.kr>
2024-04-19 16:05:21 +0900

    Initialize Project

---
+++ LifecycleDemo/LifecycleDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifecycleDemo
+//
+//  Created by Jungman Bae on 4/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifecycleDemo/LifecycleDemo/LifecycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleDemoApp.swift
+//  LifecycleDemo
+//
+//  Created by Jungman Bae on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LifecycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
