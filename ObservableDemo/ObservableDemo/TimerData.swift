12105b0685822c666c541b7d2120cb88cd8b0757
jmbae <jmbae@codegrove.co.kr>
2024-04-19 17:15:24 +0900

    TimerData

--- ObservableDemo/ObservableDemo/TimerData.swift
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Combine
+
+class TimerData: ObservableObject {
+    @Published var timeCount = 0
+    var timer: Timer?
+    
+    init() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(timerDidFire),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @objc func timerDidFire() {
+        timeCount += 1
+    }
+    
+    func resetCount() {
+        timeCount = 0
+    }
+}
