788bce94afb262e705063b426677d7362ca09736
jmbae <jmbae@codegrove.co.kr>
2024-04-19 11:35:47 +0900

    Task async/await

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -10,8 +10,25 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-
+            Button(action: {
+                doSomething()
+            }) {
+                Text("Do something")
+            }
         }
+    }
+    
+    func doSomething() {
+        print("Start \(Date())")
+        Task {
+            await takesTooLong()
+        }
+        print("End \(Date())")
+    }
+    
+    func takesTooLong() async {
+        sleep(5)
+        print("Async task completed at \(Date())")
     }
 }
 
