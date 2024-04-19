1ddbe28c749990e34140b3866c377d960b57d715
jmbae <jmbae@codegrove.co.kr>
2024-04-19 17:22:58 +0900

    ContentView add NavigationStack and TimerData

--- ObservableDemo/ObservableDemo/ContentView.swift
+++ ObservableDemo/ObservableDemo/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var timerData: TimerData = TimerData()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Timer count = \(timerData.timeCount)")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                Button(action: resetCount) {
+                    Text("Reset Counter")
+                }
+            }
         }
-        .padding()
+    }
+    
+    func resetCount() {
+        timerData.resetCount()
     }
 }
 
--- ObservableDemo/ObservableDemo/TimerData.swift
+++ ObservableDemo/ObservableDemo/TimerData.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jungman Bae on 4/19/24.
 //
-
 import Foundation
 import Combine
 
