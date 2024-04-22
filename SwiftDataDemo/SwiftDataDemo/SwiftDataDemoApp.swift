10126e4480c3b51f71a5a4743c668b5a9b1e1b4e
jmbae <jmbae@codegrove.co.kr>
2024-04-22 13:05:03 +0900

    Initialize Project

---
+++ SwiftDataDemo/SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Jungman Bae on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
