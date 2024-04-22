93a2f91073fb52d3d7b6052cca0df3460aec1646
jmbae <jmbae@codegrove.co.kr>
2024-04-22 09:10:40 +0900

    Initialize Project StorageDemo

---
+++ StorageDemo/StorageDemo/StorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageDemoApp.swift
+//  StorageDemo
+//
+//  Created by Jungman Bae on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct StorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
