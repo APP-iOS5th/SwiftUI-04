1d49a74acea321006f0b0df755acb7f65512c362
jmbae <jmbae@codegrove.co.kr>
2024-04-23 09:05:02 +0900

    Initialize Project

---
+++ ColorMemo/ColorMemo/ColorMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMemoApp.swift
+//  ColorMemo
+//
+//  Created by Jungman Bae on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMemo/ColorMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorMemo
+//
+//  Created by Jungman Bae on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
